4d81f6d32f2cc862083553a7742e9e992372cb48
mykaella710-png <mykaella710@gmail.com>
2026-09-20 19:28:08 -0600

    Timers and Timing Activity
    Practiced using time functions

---
+++ root/extension.py
@@ -0,0 +1,31 @@
+import time
+import random
+
+def delay(s,l):
+    while True:
+        elapsed = time.time() - s
+        if elapsed > l:
+            break
+        time.sleep(0.1)
+
+def go_and_stop():
+    print ("go")
+    start = time.monotonic()
+    input("enter")
+    stop = time.monotonic()
+    reaction = stop - start
+    return reaction
+
+counter = 0
+lowest = None
+while counter < 5:
+    start = time.time()
+    timer_length = random.randint(2,5)
+    delay(start,timer_length)
+    reaction = go_and_stop()
+    print(reaction)
+    counter += 1
+    if lowest is None or reaction < lowest:
+        lowest = reaction
+print("Game Over")
+print("Your highscore is:", lowest)
